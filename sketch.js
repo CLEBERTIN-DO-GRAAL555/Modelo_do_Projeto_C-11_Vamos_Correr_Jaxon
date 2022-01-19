3c4f8608a514a4bb1200dea7a301d5ce0ec42e85
CLEBERTIN-DO-GRAAL555 <92889323+CLEBERTIN-DO-GRAAL555@users.noreply.github.com>
2022-01-19 16:51:38 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
-
+var meninoimg,menino
+var chaoimg,chao
 function preload(){
+  meninoimg=loadAnimation("Runner-1.png","Runner-2.png");
+  chaoimg=loadImage("path.png");
   //imagens pré-carregadas
 }
 
 function setup(){
   createCanvas(400,400);
+  chao=createSprite(200,200);
+  menino=createSprite(180,300);
+  chao.addImage(chaoimg)
+  menino.addAnimation("menino",meninoimg)
   //crie sprite aqui
 }
 
 function draw() {
   background(0);
+  drawSprites();
 
 }
